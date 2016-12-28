1a81ed77ea7d9b12989152f13e0f437db22133f9
amhayslip <amhayslip@gmail.com>
2016-12-28 19:42:26 +0200

    add input bar

---
+++ src/components/input_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class InputBar extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { url: '' }
+  }
+
+  handleClick () {
+    this.props.onShortcutSubmit(this.state.url)
+  }
+
+  render () {
+    return (
+      <div>
+        <input
+          value={this.state.url}
+          onChange={event => this.setState({url: event.target.value})} />
+        <button onClick={() => this.handleClick()}>submit</button>
+      </div>
+    )
+  }
+
+}
+
+export default InputBar
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import InputBar from './components/input_bar'
 
 
 class App extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      shortcuts: []
+    }
+  }
+
+  addShortcut (shortcut) {
+    const newShortcut = {
+      url: shortcut
+    }
+
+    this.setState({
+      shortcuts: this.state.shortcuts.concat([newShortcut])
+    })
   }
 
   render () {
     return (
       <div>
-        Hello World
-        // InputBox
-        // ShortcutList
-        //// ShortcutItem
+        <InputBar onShortcutSubmit={shortcut => this.addShortcut(shortcut)} />
+        <ShortcutList shortcuts={this.state.shortcuts} />
       </div>
     )
   }
