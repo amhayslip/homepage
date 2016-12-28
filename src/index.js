70abdf558fc29904cb8ad1dbe4b7ec8afe7f84f3
amhayslip <amhayslip@gmail.com>
2016-12-28 17:08:24 +0200

    initialize project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        Hello World
+        // InputBox
+        // ShortcutList
+        //// ShortcutItem
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
